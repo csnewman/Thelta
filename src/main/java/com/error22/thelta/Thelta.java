e00a78914817e19be234dc285853baa91466be44
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-12-17 17:41:00 +0000

    Continued work on computers

--- src/main/java/com/error22/thelta/Thelta.java
+++ src/main/java/com/error22/thelta/Thelta.java
@@ -1,12 +1,8 @@
 package com.error22.thelta;
 
-import com.error22.thelta.computers.BlockComputer;
 import com.error22.thelta.computers.TheltaComputers;
-import com.error22.thelta.testing.QuickItem;
 import com.error22.thelta.testing.TheltaTesting;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -14,7 +10,6 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Thelta.MODID, version = Thelta.VERSION)
 public class Thelta {
--- src/main/java/com/error22/thelta/computers/TheltaComputers.java
+++ src/main/java/com/error22/thelta/computers/TheltaComputers.java
@@ -3,15 +3,16 @@ package com.error22.thelta.computers;
 import com.error22.thelta.Thelta;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFurnace;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class TheltaComputers {
-	public static BlockComputer testComputer;
+	public static BasicMonitor basicMonitor;
 
 	public static void preInit() {
-		testComputer = new BlockComputer("computer_test");
-		registerSimpleBlock(testComputer);
+		basicMonitor = new BasicMonitor("basicMonitor");
+		registerSimpleBlock(basicMonitor);
 	}
 
 	public static void init() {
