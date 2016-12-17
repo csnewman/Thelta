5ce6dc58b4a2e194df6e7061e6090e9d718957ed
rater193 <rater193@gmail.com>
2016-12-17 12:57:48 -0500

    Added names to the en_US language

--- src/main/java/com/error22/thelta/minivox/blocks/QuickBlock.java
+++ src/main/java/com/error22/thelta/minivox/blocks/QuickBlock.java
@@ -4,16 +4,24 @@ import com.error22.thelta.minivox.Minivox;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class QuickBlock extends Block {
 	
+	public ItemBlock itemBlock;
+	
 	public QuickBlock(String itemname) {
 		super(Material.ROCK);
+		
 		setUnlocalizedName(itemname);
 		setRegistryName(itemname);
 		setCreativeTab(Minivox.creativetab);
 		GameRegistry.register(this);
+		
+		itemBlock = new ItemBlock(this);
+		itemBlock.setRegistryName(getRegistryName());
+		GameRegistry.register(itemBlock);
 	}
 
 }
