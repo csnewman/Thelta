6422502749ee984eb85f18e5c81eb2dc3cf116c7
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-12-17 15:52:23 +0000

    Applied thelta naming

---
+++ src/main/java/com/error22/thelta/Thelta.java
@@ -0,0 +1,18 @@
+package com.error22.thelta;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Thelta.MODID, version = Thelta.VERSION)
+public class Thelta {
+	public static final String MODID = "thelta";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+		
+		
+	}
+}
