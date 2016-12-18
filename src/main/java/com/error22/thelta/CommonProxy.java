c055150c0bebd213b9c3178c12fa76dd3f7d6595
rater193 <rater193@gmail.com>
2016-12-17 21:26:14 -0500

    Added CraftingComponents to let us add all our useless crafting ingredient items in 1 place.

--- src/main/java/com/error22/thelta/CommonProxy.java
+++ src/main/java/com/error22/thelta/CommonProxy.java
@@ -1,6 +1,7 @@
 package com.error22.thelta;
 
 import com.error22.thelta.computers.TheltaComputers;
+import com.error22.thelta.craftingcomponents.CraftingComponents;
 import com.error22.thelta.minivox.Minivox;
 
 import net.minecraft.item.Item;
@@ -10,16 +11,19 @@ public class CommonProxy {
 	public void preInit() {
 		TheltaComputers.preInit();
 		Minivox.preInit();
+		CraftingComponents.preInit();
 	}
 
 	public void init() {
 		TheltaComputers.init();
 		Minivox.init();
+		CraftingComponents.init();
 	}
 
 	public void postInit() {
 		TheltaComputers.postInit();
 		Minivox.postInit();
+		CraftingComponents.postInit();
 	}
 
 }
---
+++ src/main/java/com/error22/thelta/craftingcomponents/CraftingComponents.java
@@ -0,0 +1,23 @@
+package com.error22.thelta.craftingcomponents;
+
+public class CraftingComponents {
+	public static void preInit() {
+		
+	}
+	
+	public static void init() {
+		
+	}
+	
+	public static void postInit() {
+		
+	}
+	
+	public static void registerItemRenderers() {
+		
+	}
+	
+	public static void registerBlockRenderers() {
+		
+	}
+}
