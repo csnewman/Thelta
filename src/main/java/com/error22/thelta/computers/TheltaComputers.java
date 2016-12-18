0a9e1dc033196aee34ff8c64a9e310037e51e2aa
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-12-18 17:37:15 +0000

    Fixed model registration

--- src/main/java/com/error22/thelta/computers/TheltaComputers.java
+++ src/main/java/com/error22/thelta/computers/TheltaComputers.java
@@ -2,12 +2,10 @@ package com.error22.thelta.computers;
 
 import com.error22.thelta.Thelta;
 import com.error22.thelta.pipeline.Pass;
-import com.error22.thelta.pipeline.PipelineStage;
 import com.error22.thelta.pipeline.Stage;
 import com.error22.thelta.pipeline.StageMethod;
 import com.error22.thelta.pipeline.Stages;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.model.ModelLoader;
@@ -21,13 +19,18 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class TheltaComputers {
 	public static final String monitorStage = "computers-monitors";
 	public static final String renderersStage = "computers-renderers";
-	private static BasicMonitor basicMonitor;
+	public static BasicMonitor basicMonitor;
+	public static ItemBlock basicMonitorItem;
 
 	@StageMethod(stage = monitorStage, pass = Pass.PreInit)
 	private static void registerMonitors() {
 		basicMonitor = new BasicMonitor("computer_monitor_basic");
-		registerSimpleBlock(basicMonitor);
+		GameRegistry.register(basicMonitor);
 		GameRegistry.registerTileEntity(TileEntityComputerMonitor.class, "thelta_computer_monitor");
+
+		basicMonitorItem = new ItemBlock(basicMonitor);
+		basicMonitorItem.setRegistryName(basicMonitor.getRegistryName());
+		GameRegistry.register(basicMonitorItem);
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -35,15 +38,8 @@ public class TheltaComputers {
 	private static void registerClientRenderers() {
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityComputerMonitor.class,
 				new TileEntityComputerMonitorRenderer());
-	}
 
-	private static void registerSimpleBlock(Block block) {
-		ItemBlock item = new ItemBlock(block);
-		item.setRegistryName(block.getRegistryName());
-		GameRegistry.register(block);
-		GameRegistry.register(item);
-		ModelLoader.setCustomModelResourceLocation(item, 0,
-				new ModelResourceLocation(Thelta.MODID + ":" + block.getRegistryName().getResourcePath(), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(basicMonitorItem, 0, new ModelResourceLocation(
+				Thelta.MODID + ":" + basicMonitor.getRegistryName().getResourcePath(), "inventory"));
 	}
-
 }
