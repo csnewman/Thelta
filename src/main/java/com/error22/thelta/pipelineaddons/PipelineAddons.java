764e67920e4fd5c63867de1669771aa7dc2f1f49
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-07-29 10:28:03 +0100

    Remove pipeline addons

--- src/main/java/com/error22/thelta/pipelineaddons/PipelineAddons.java
+++
@@ -1,9 +0,0 @@
-package com.error22.thelta.pipelineaddons;
-
-public class PipelineAddons {
-	/*
-	 * The purpos of this class is to make it easier to tap into things
-	 * such as being able to have code run on the game loop, or render loop
-	 * or even other things.
-	 */
-}
